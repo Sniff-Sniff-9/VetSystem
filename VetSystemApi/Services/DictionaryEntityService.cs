3a89a09cb47e342806c4a1e97b8f924b6ceb0c37
Sniff-Sniff-9 <babayka.07@mail.ru>
2026-02-10 02:27:01 +0300

    DictionaryEntityService

---
+++ VetSystemApi/Services/DictionaryEntityService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using VetSystemInfrastructure.Configuration;
+using VetSystemModels.Dto;
+using VetSystemModels.Entities;
+
+namespace VetSystemApi.Services
+{
+    public class DictionaryEntityService<TEntity> where TEntity : class
+    {
+        private readonly AppDbContext _context;
+
+        public DictionaryEntityService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            var entities = await _context.Set<TEntity>().ToListAsync();
+            return entities;
+        }
+
+    }
+}
